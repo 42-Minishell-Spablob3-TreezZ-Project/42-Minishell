3fbebdc2fb12904a6ce1023aed35c9929f24d627
TreezZ <gonzosbl@gmail.com>
2025-11-28 14:49:36 +0000

    Began treating sent commands, beginning tokenization.

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -1,8 +1,56 @@
 #include "../includes/minishell.h"
 
+//Mode for treating stuff with quotes, and without
+
+/* check_token_word() */
+/* { */
+	
+/* } */
+
+/* wejh90df */
+
+/* int	tokenize(char **split) */
+/* { */
+/* 	while (*split) */
+/* 	{ */
+		
+/* 		split++; */
+/* 	} */
+/* 	return (1); */
+/* } */
+
+char	*parse_command(char *cmd)
+{
+	int		i;
+	char	**results;
+
+	i = 0;
+	results = ft_split(cmd, ' ');
+	while (results[i])
+		i++;
+	if (i == 1)
+		return (results[0]);
+	if (i > 1)
+	{
+		return ("FALTA ISTO");
+	}
+	return NULL;
+}
+
 int	class_command(char *cmd)
 {
-	if (ft_strncmp (cmd, "exit", INT_MAX) == 0)
+	char	*parsed;
+
+	parsed = parse_command(cmd);
+	if (!parsed)
+		return (1);
+	if (ft_strncmp (parsed, "exit", INT_MAX) == 0)
 		return (0);
+//If invalid command
+	else if (parsed)
+	{
+		ft_printf("%s: command not found\n", parsed);
+		return (1);
+	}
 	return (1);
 }
