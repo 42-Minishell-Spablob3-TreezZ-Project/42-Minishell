bec56835e3fd6f7a8855c2edcfae6e0fc957e75a
Joao Pedro De Mesquita Fernandes <joapedro@c1r12s6.42porto.com>
2025-12-09 12:34:38 +0000

    echo and env

--- src/built-in/echo.c
+++ src/built-in/echo.c
@@ -5,36 +5,44 @@ int	check_n_flag(char *argv)
 	int	i;
 
 	i = 0;
-	if (argv[0] == '-' && argv[1] == 'n')
-		return (1);
-	while (argv[i])
-	{
-		if (argv[i] != 'n')
-			return (0);
+	if (!argv || argv[0] != '-' || argv[1] != 'n')
+		return (0);
+	i = 1;
+	while (argv[i] == 'n')
 		i++;
-	}
-	return (1);
+	if (!argv[i])
+		return (1);
+	if (argv[i] != 'n')
+		return (0);
+	else
+		return(1);
 }
 
 
 void	echo_builtin(char **argv)
 {
 	int i;
-	int newline;
+	int n_flag;
 
-	i = 1;
-	newline = 0;
+	i = 0;
+	n_flag = 0;
 
-	while (argv[i] && check_n_flag(argv[i]))
-	{
-		newline = 1;
-		i++;
-	}
+	if (argv[i] && check_n_flag(argv[i]))
+		n_flag = 1;
 	while (argv[i])
 	{
 		printf("%s", argv[i]);
 		i++;
 	}
-	if (newline)
+	if (!n_flag)
 		printf("\n");
 }
+
+
+/* 
+TESTAGEM SEM INPUT VERDADEIRO
+int main(void)
+{
+	char *str[] = {"-n", NULL};
+	echo_builtin(str);
+} */
