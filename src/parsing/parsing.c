d6ca576a289d517269f642be57899a0d126462d7
TreezZ <treezz-dev@the-wired.org>
2025-12-18 15:59:45 +0000

    Progress on implementing lexer. Not finished.

--- src/parsing/parsing.c
+++ src/parsing/parsing.c
@@ -1,45 +1,40 @@
 #include "../includes/minishell.h"
 
+/* void	start_lexer(t_tokens **tokens, char *cmd) */
+/* { */
+/* 	int			i; */
+/* 	t_tokens	*new; */
+
+/* 	i = 0; */
+/* 	new = NULL; */
+/* 	while (cmd[i]) */
+/* 	{ */
+/* 		if (cmd[i] == 32 || (cmd[i] >= 9 && cmd[i] <= 13)) */
+/* 			i++; */
+/* 		is_operator(cmd[i]); */
+/* 			tokenize_operator(cmd, new, &i); */
+/* 		else */
+/* 			tokenize_word(cmd, new, &i); */
+/* 	} */
+/* } */
+
 void	start_lexer(t_tokens **tokens, char *cmd)
 {
 	int			i;
+	int			which;
 	t_tokens	*new;
 
 	i = 0;
 	new = NULL;
 	while (cmd[i])
 	{
-		if (cmd[i] == 32 || (cmd[i] >= 9 && cmd[i] <= 13))
-			i++;
-		is_operator(cmd[i]);
+		which = which_token(cmd, &i);
+		if (!which)
 			tokenize_operator(cmd, new, &i);
 		else
 			tokenize_word(cmd, new, &i);
+		add_token(tokens, new);
 	}
-}
-
-int is_operator(char **results)
-{
-	int	i;
-
-	i = 0;
-	while (results[i])
-	{
-		if (!ft_strncmp(results[i], "|", INT_MAX))
-			printf("TOKEN_PIPE\n");
-		else if (!ft_strncmp(results[i], ">", INT_MAX))
-			printf("TOKEN_OUT\n");
-		else if (!ft_strncmp(results[i], "<", INT_MAX))
-			printf("TOKEN_IN\n");
-		else if (!ft_strncmp(results[i], ">>", INT_MAX))
-			printf("TOKEN_APPEND\n");
-		else if (!ft_strncmp(results[i], "<<", INT_MAX))
-			printf("TOKEN_HEREDOC\n");
-		else
-			printf ("TOKEN_WORD\n");
-		i++;
-	}
-	return (0);
 }
 
 char	*parse_command(char *cmd)
@@ -53,10 +48,8 @@ char	*parse_command(char *cmd)
 		return NULL;
 	//Initialize token struct with all values to NULL
 	tokens = ft_calloc(1, sizeof(tokens));
-	start_lexer(&tokens, cmd);
-	is_operator(results);
-
-	return (results[0]);
+//	start_lexer(&tokens, cmd);
+	return (tokens->input);
 }
 
 int	class_command(char *cmd)
--- src/parsing/parsing_utils.c
+++ src/parsing/parsing_utils.c
@@ -4,3 +4,23 @@ void	tokenize_operator(char *cmd, t_tokens *new, int *i)
 {
 	
 }
+
+int	which_token(char *cmd, int *i)
+{
+	if (cmd[*i] == 32 || (cmd[*i] >= 9 && cmd[*i] <= 13))
+		(*i)++;
+	else if (cmd[*i] == '|' || cmd[*i] == '>' || cmd[*i] == '<')
+		return (0);
+	return (*i);
+}
+
+//#include <stdio.h>
+
+/* int	main(void) */
+/* { */
+/* 	char	*str = "hello > yo"; */
+/* 	int	i = 0; */
+/* 	while (str[i]) */
+/* 		printf("%i\n", which_token(str, &i)); */
+/* 	return (0); */
+/* } */
