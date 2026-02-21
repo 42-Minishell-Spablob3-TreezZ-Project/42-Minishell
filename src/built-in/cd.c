a444e531f537583301eea751ac5dad60ce0ad88e
spablob3 <joapedro@student.42porto.com>
2026-02-21 20:06:18 +0000

    need to add OLDPWD to env list (node)

--- src/built-in/cd.c
+++ src/built-in/cd.c
@@ -15,28 +15,30 @@
 int	cd_builtin(t_command *cmd, t_env **env)
 {
 	char	*path;
-	//char	*oldpwd;
-	//char	*pwd;
-	
-	//pwd = getcwd(NULL, 0);
+	char	*pwd;
+	char	*curr_dir;
+
+	pwd = getcwd(NULL, 0);
 	if (!cmd->argv[1] || ft_strncmp(cmd->argv[1], "~", INT_MAX) == 0)
-		path = getenv("HOME");
+	path = getenv("HOME");
 	else if (ft_strncmp(cmd->argv[1], "-", INT_MAX) == 0)
 		path = getenv("OLDPWD");
 	else
 		path = cmd->argv[1];
-
+		
 	if (chdir(path) != 0)
 	{
 		printf ("cd: %s: No such file or directory\n", path);
 		return (1);
 	}
-	update_env_var(env, path);
+	curr_dir = getcwd(NULL, 0);
+	update_env_var(env, pwd, curr_dir);
 	printf("PRINT do path: %s\n", path);
+	printf("current dir: %s\n", pwd);
 	return (0);
 }
 
-void	update_env_var(t_env **env, char *path)
+void	update_env_var(t_env **env, char *oldpwd, char *pwd)
 {
 	t_env *temp;
 
@@ -46,10 +48,11 @@ void	update_env_var(t_env **env, char *path)
 		if (ft_strncmp(temp->key, "PWD", 3) == 0)
 		{
 			free(temp->value);
-			temp->value = ft_strdup(path);
+			temp->value = ft_strdup(pwd);
 		}
 		temp = temp->next;
 	}
+	//adicionar node OLDPWD à env list.
 }
 
 //Implementar "cd -" -> vai para diretorio anterior.
